d567104efe42ac02f873415d69ad658e3b69abbe
MatthewChoi <matthewchoi95@gmail.com>
2020-02-14 17:34:43 +0800

    Updated files

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 import * as express from 'express';
-import * as Knex from 'knex';
+// import * as Knex from 'knex';
 
-const knexConfig = require('./knexfile');
-const knex = Knex(knexConfig('development'));
+// const knexConfig = require('./knexfile');
+// const knex = Knex(knexConfig[process.env.NODE_ENV || "development"])
 
 
 
 
 const app = express();
+
+app.use(express.static('public'));
+
 
 
 

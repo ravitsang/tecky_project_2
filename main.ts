22952c5b03102ed7778f210e1149b8f6faec470b
MatthewChoi <matthewchoi95@gmail.com>
2020-02-18 01:20:49 +0800

    Added DB full text search function

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as expressSession from 'express-session';
 import * as bodyParser from 'body-parser';
 import { UserRouter } from './routers/userRouter';
+import { SearchRouter } from './routers/SearchRouter';
 // import * as Knex from 'knex';
 
 // const knexConfig = require('./knexfile');
@@ -23,6 +24,7 @@ app.use(express.static('public'));
 
 app.post('/api/v1/login', new UserRouter().login);
 app.post('/api/v1/register', new UserRouter().register);
+app.get('/api/v1/search', new SearchRouter().search);
 
 app.use((req, res, next) => {
     if (req.session?.isLogin) {

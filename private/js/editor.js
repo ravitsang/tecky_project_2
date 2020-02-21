f1a5a3972a1946f2be29b2982dc854ec01923656
lexingtonc852 <lexingtonc852@gmail.com>
2020-02-21 17:51:36 +0800

    Updated ready-to-use editor

--- private/js/editor.js
+++ private/js/editor.js
@@ -26,11 +26,7 @@ window.onload=()=>{
         _initRequest() {
             const xhr = this.xhr = new XMLHttpRequest();
     
-            // Note that your request may look different. It is up to you and your editor
-            // integration to choose the right communication channel. This example uses
-            // a POST request with JSON as a data structure but your configuration
-            // could be different.
-            xhr.open( 'POST', 'http://localhost:8080/editor/create', true );
+            xhr.open( 'POST', 'http://localhost:8080/editor/upload', true );
             xhr.responseType = 'json';
         }
     
@@ -45,28 +41,15 @@ window.onload=()=>{
             xhr.addEventListener( 'load', () => {
                 const response = xhr.response;
     
-                // This example assumes the XHR server's "response" object will come with
-                // an "error" which has its own "message" that can be passed to reject()
-                // in the upload promise.
-                //
-                // Your integration may handle upload errors in a different way so make sure
-                // it is done properly. The reject() function must be called when the upload fails.
                 if ( !response || response.error ) {
                     return reject( response && response.error ? response.error.message : genericErrorText );
                 }
     
-                // If the upload is successful, resolve the upload promise with an object containing
-                // at least the "default" URL, pointing to the image on the server.
-                // This URL will be used to display the image in the content. Learn more in the
-                // UploadAdapter#upload documentation.
                 resolve( {
                     default: response.url
                 } );
             } );
     
-            // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
-            // properties which are used e.g. to display the upload progress bar in the editor
-            // user interface.
             if ( xhr.upload ) {
                 xhr.upload.addEventListener( 'progress', evt => {
                     if ( evt.lengthComputable ) {
@@ -84,11 +67,6 @@ window.onload=()=>{
     
             data.append( 'upload', file );
     
-            // Important note: This is the right place to implement security mechanisms
-            // like authentication and CSRF protection. For instance, you can use
-            // XMLHttpRequest.setRequestHeader() to set the request headers containing
-            // the CSRF token generated earlier by your application.
-    
             // Send the request.
             this.xhr.send( data );
         }
@@ -99,7 +77,6 @@ window.onload=()=>{
     function MyCustomUploadAdapterPlugin( editor ) {
         editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
             // Configure the URL to the upload script in your back-end here!
-            
             return new MyUploadAdapter( loader );
         };
     }
@@ -107,12 +84,6 @@ window.onload=()=>{
     let editor;
     BalloonEditor
     .create(document.querySelector('#editor'),{
-        // autosave: {
-        //     waitingTime: 5000, // in ms
-        //     save( editor ) {
-        //         return saveData(editor.getData());
-        //     }
-        // },
         extraPlugins: [ MyCustomUploadAdapterPlugin ],
         toolbar: {
             items: [
@@ -163,52 +134,31 @@ window.onload=()=>{
         console.error('There was a problem initializing the editor.',error);
     });
 
-    // function saveData( data ) {
-    //     return new Promise( resolve => {
-    //         setTimeout( () => {
-    //             console.log( 'Saved', data );
-    
-    //             resolve();
-    //         }, HTTP_SERVER_LAG );
-    //     } );
-    // }
-
-    // function displayStatus( editor ) {
-    //     const pendingActions = editor.plugins.get( 'PendingActions' );
-    //     const statusIndicator = document.querySelector( '#editor-status' );
-    
-    //     pendingActions.on( 'change:hasAny', ( evt, propertyName, newValue ) => {
-    //         if ( newValue ) {
-    //             statusIndicator.classList.add( 'busy' );
-    //         } else {
-    //             statusIndicator.classList.remove( 'busy' );
-    //         }
-    //     } );
-    // }
 
     document.querySelector('#submit-btn')
         .addEventListener('click',async (event)=>{
             const editorData = await editor.getData();
-            console.log(editorData)
+            const title = editorData.match(/<h1>.*<\/h1>(.*)$/)[1]
+            const content = editorData.match(/<h1>(.*)<\/h1>/)[1]
 
-            let domParser = new DOMParser();
-            let doc = domParser.parseFromString(editorData,'text/html')
-            const title = doc.querySelector('h1').outerHTML
-            console.log(title)
-            
-            const res = await fetch ('/editor/create',{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body: JSON.stringify({
-                    title:title,
-                    content:editorData
-                })
-            });
-            console.log(res);
+            // let domParser = new DOMParser();
+            // let doc = domParser.parseFromString(editorData,'text/html');
+            //     const title = doc.querySelector('h1').textContent;
+            //     console.log(title)
+                
+                const res = await fetch ('/editor/create',{
+                    method:"POST",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body: JSON.stringify({
+                        title:title,
+                        content:content
+                    })
+                });
+                const result = await res.json();
+                if (result.id) {
+                    window.location = "/m";
+                }
         })
-
-    
-
 }

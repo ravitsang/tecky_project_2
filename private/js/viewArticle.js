ea8b3d38807a137b223fa69aeb04a79810f6cfec
MatthewChoi <matthewchoi95@gmail.com>
2020-02-23 17:58:20 +0800

    Added viewArticle.html (user details)

--- private/js/viewArticle.js
+++ private/js/viewArticle.js
@@ -1,19 +1,19 @@
-window.onload = async () =>{
+window.onload = async () => {
 
     const url = window.location.search;
-   
+
     const id = parseInt(url.split('=')[1]);
 
-    
+
     const res = await fetch(`/article/viewArticle?articleId=${id}`)
 
     const result = await res.json();
 
-        
+
     console.log(result);
 
-    console.log(result.article[0]);
-    console.log(result.authorName);
+    // console.log(result.article[0]);
+    // console.log(result.authorName);
 
     const article = result.article[0];
 
@@ -24,17 +24,27 @@ window.onload = async () =>{
 
     let photo = article.content.split('</noscript>')[0].split('<noscript>')[1];
 
-    
-    if (!photo){
+
+    if (!photo) {
         photo = "";
+    }
+    if (result.authorName.authorPhoto === null) {
+        result.authorName.authorPhoto = `<img src="./images/1_dmbNkD5D-u45r44go_cf0g.png">`;
     }
 
 
-
     document.querySelector('.title').innerHTML = article.title;
-    document.querySelector('.user').innerHTML += result.authorName;
-    document.querySelector('.article-details').innerHTML += articleCreateDate;
-    document.querySelector('.article-details').innerHTML += ` ${article.reading_time} min read`;
+    document.querySelector('.article-details').innerHTML = `               
+    <div class="article-image">${result.authorName.authorPhoto}</div>
+    <div class="article-details">
+        <div class="article-user">
+            <div class="article-authorName">${result.authorName.authorName}</div>
+        </div>
+        <div class="article-detail d-flex">
+            <div class="article-date">${articleCreateDate}</div>
+            <div class="article-readtime">${article.reading_time} min read</div>
+        </div>
+    </div>`;
     document.querySelector('.photo').innerHTML = photo;
     document.querySelector('.content').innerHTML = article.content;
 
